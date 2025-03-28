dcd3779635517e24f70bd07114b9e256b5d60af1
APIVerve Team <hello@apiverve.com>
2025-03-28 18:02:51 -0500

    Bug Fixes

--- src/apiverve_usernamegenerator/apiClient.py
+++ src/apiverve_usernamegenerator/apiClient.py
@@ -10,10 +10,10 @@ class UsernamegeneratorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/usernamegenerator'
+        self.base_url = 'https://api.apiverve.com/v1/usernamegenerator'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
